f5defd0dfffedf2c34ae0bbc08d712a2a70d89b7
Harriet Dashnow <h.dashnow@gmail.com>
2015-04-15 09:33:36 +1000

    Started adding pipeline stages

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,4 +1,26 @@
 
+index_DB = {
+    exec """
+        bowtie2-build $input.fasta $output.fasta.prefix
+    """
+}
+
+map_reads = {
+    exec """
+       /vlsci/VR0002/shared/hdashnow 
+    """
+}
+
+extract_reads = {
+    exec """
+    """
+}
+
+assemble = {
+    exec """
+    """
+}
+
 run {
     index_DB +
     "%_*.fastq.gz" * [ map_reads +
