489eaf49306694237089fc4274f3d40d7b9cb9c2
Harriet Dashnow <h.dashnow@gmail.com>
2015-04-15 10:52:15 +1000

    Stage index_DB working

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,12 +1,19 @@
 
 index_DB = {
+    transform( '.1.bt2', 
+                '.2.bt2', 
+                '.3.bt2', 
+                '.4.bt2', 
+                '.rev.1.bt2', 
+                '.rev.2.bt2') {
     exec """
-        bowtie2-build $input.fasta $output.fasta.prefix
+        bowtie2-build $input.fna $output1.prefix.prefix
     """
+    }
 }
 
 map_reads = {
-    transform("1.ebwt","fastq.gz","fastq.gz") {
+    transform("1.bt2","fastq.gz","fastq.gz") {
         exec """
             bowtie2 
             -1 $input1 -2 $input2 
@@ -31,8 +38,8 @@ assemble = {
 }
 
 run {
-    index_DB +
-    "%_*.fastq.gz" * [ map_reads +
-    extract_reads +
-    assemble ]
+    index_DB //+
+//    "%_*.fastq.gz" * [ map_reads +
+//    extract_reads +
+//    assemble ]
 }
